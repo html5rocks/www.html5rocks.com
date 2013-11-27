ebcc3c1a3327802734460f017f7bf70b6b0fb399
Paul Kinlan <paul.kinlan@gmail.com>
2013-11-27 13:38:59 -0800

    #773 - adding a new script that simplifies the deployment to a version on GAE

---
+++ scripts/update_site_oauth_target.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Compresses the site's JS/CSS and cache busts links before uploading the app
+# to App Engine.
+# 
+# Note: This script should be used in place of using appcfg.py update directly
+# to update the application on App Engine.
+#
+# Copyright 2012 Eric Bidelman <ericbidelman@html5rocks.com>
+
+versionStr=${1:-master}
+
+./compress_js_css.sh
+appcfg.py --oauth2 --version=$versionStr update ../
+
